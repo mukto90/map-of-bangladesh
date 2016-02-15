0449535d1198f8ad9d3ddab6b5dcf684594c1003
mukto90 <n.mukto@gmail.com>
2016-02-16 02:13:15 +0600

    Sample CSS and JS added

---
+++ script.js
@@ -0,0 +1,25 @@
+$ = new jQuery.noConflict();
+$(document).ready(function(){
+	// some basic events
+	$("#division-dhaka").click(function(){
+		window.location.href="https://www.google.com/search?q=dhaka"
+	})
+	$("#division-chittagong").click(function(){
+		window.location.href="https://www.google.com/search?q=chittagong"
+	})
+	$("#division-sylhet").click(function(){
+		window.location.href="https://www.google.com/search?q=sylhet"
+	})
+	$("#division-khulna").click(function(){
+		window.location.href="https://www.google.com/search?q=khulna"
+	})
+	$("#division-barishal").click(function(){
+		window.location.href="https://www.google.com/search?q=barishal"
+	})
+	$("#division-rajshahi").click(function(){
+		window.location.href="https://www.google.com/search?q=rajshahi"
+	})
+	$("#division-rangpur").click(function(){
+		window.location.href="https://www.google.com/search?q=rangpur"
+	})
+})
